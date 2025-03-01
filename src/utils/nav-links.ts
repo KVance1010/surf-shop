60edde1dc9b3ad31ead77b84763e3e43426261af
Kyle Vance <vanceofalifetime@protonmail.com>
2025-02-28 16:59:59 -0800

    fixed the links and create branches

--- src/utils/nav-links.ts
+++ src/utils/nav-links.ts
@@ -1,70 +1,65 @@
 interface DropdownItem {
-    title: string;
-    href: string;
-    description: string;
-  }
-  
- export const videoDropdown: DropdownItem[] = [
-    {
-      title: "Most Popular Videos",
-      href: "/video1",
-      description:
-        "A modal dialog that interrupts the user with important content and expects a response."
-    },
-    {
-      title: "Learning Resources",
-      href: "/video2",
-      description: "For sighted users to preview content available behind a link."
-    },
-    {
-      title: "My Favorites",
-      href: "/video3",
-      description:
-        "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar."
-    }
-  ];
-  
-  export const storeDropdown: DropdownItem[] = [
-    {
-      title: "Men's apparel",
-      href: "/men",
-      description:
-        "A modal dialog that interrupts the user with important content and expects a response."
-    },
-    {
-      title: "Women's apparel",
-      href: "/women",
-      description: "For sighted users to preview content available behind a link."
-    },
-    {
-      title: "Accessories",
-      href: "/accessories",
-      description:
-        "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar."
-    },
-    {
-      title: "Surf Boards",
-      href: "/boards",
-      description:
-        "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar."
-    }
-  ];
+  title: string;
+  href: string;
+  description: string;
+}
 
-  export const navHeaders = [
-    {
-      title: "Store",
-      href: "/"
-    },
-    {
-      title: "Videos",
-      href: "/"
-    },
-    {
-      title: "About Us",
-      href: "/about"
-    },
-    {
-      title: "Contact Us",
-      href: "/contact"
-    }
-  ];
+export const storeDropdown: DropdownItem[] = [
+  {
+    title: "Men's apparel",
+    href: "/mens-apparel",
+    description: "Explore our collection of men's apparel."
+  },
+  {
+    title: "Women's apparel",
+    href: "/womens-apparel",
+    description: "Discover the latest trends in women's fashion."
+  },
+  {
+    title: "Accessories",
+    href: "/accessories",
+    description: "Find the perfect accessories to complement your style."
+  },
+  {
+    title: "Surf Boards",
+    href: "/surf-boards",
+    description: "Browse our selection of high-quality surf boards."
+  }
+];
+
+export const videoDropdown: DropdownItem[] = [
+  {
+    title: "Most Popular Videos",
+    href: "/video1",
+    description: "Watch the most popular videos in our collection."
+  },
+  {
+    title: "Learning Resources",
+    href: "/video2",
+    description: "Access a variety of learning resources and tutorials."
+  },
+  {
+    title: "My Favorites",
+    href: "/video3",
+    description: "View and manage your favorite videos."
+  }
+];
+
+export const navHeaders = [
+  {
+    title: "Store",
+    href: "/"
+  },
+  {
+    title: "Videos",
+    href: "/"
+  },
+  {
+    title: "Messages",
+    href: "/messages"
+  },
+  {
+    title: "Map",
+    href: "/map"
+  }
+];
